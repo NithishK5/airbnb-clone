6c4852e0333d58305d79caab1d8a9436a35e069e
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2024-04-02 13:15:24 +0530

    login started

---
+++ app/(modals)/login.tsx
@@ -0,0 +1,36 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/styles";
+
+const Page = () => {
+  useWarmUpBrowser();
+  return (
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 26,
+  },
+});
+
+export default Page;
